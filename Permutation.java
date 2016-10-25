ffcafcd1b7dbd011ee8490d07d0215f02f2c551b
lynquantumman <lyn2511757@gmail.com>
2016-10-25 20:38:45 +0800

    no message

--- Permutation.java
+++ Permutation.java
@@ -1,39 +1,66 @@
-package leetcode;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Cradle Lee
- * @inputFormat 
- * 		ArrayList<Integer> prefix = new ArrayList<Integer>();
-		
-		Integer[] inputArray = {1,2,3,4};
-		LinkedList<Integer> input = new LinkedList<Integer>(Arrays.asList(inputArray));
-		
-		int n=4;
-		
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-@outputFormat	result is a ArrayList<ArrayList<Integer>>
-	and the ArrayList<Integer> in it is a permutation
  */
-import java.util.ArrayList;
-import java.util.LinkedList;
 
-public class Permutation {
-	
-	public static void Creator(ArrayList<Integer> prefix, 
-			LinkedList<Integer> input,
-			int n,
-			ArrayList<ArrayList<Integer>> result)
-	{
-		if(prefix.size()>=n){
-			result.add(prefix);
-			return;
-		}
-		int size = input.size();
-		for(int i=0;i<size;i++){
-			LinkedList<Integer> inputNext = new LinkedList<Integer>(input);
-			ArrayList<Integer> prefixPlus = new ArrayList<Integer>(prefix);
-			prefixPlus.add(inputNext.get(i));
-			inputNext.remove(i);
-			Creator(prefixPlus,inputNext,n,result);
-		}
-	}
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+    	List<List<Integer>> ans = new ArrayList<List<Integer>>();
+    	   	
+    	if(nums.length==1){
+    		ans.add(toList(nums));
+    		return ans;
+    	}
+
+    	Arrays.sort(nums);
+    	boolean changed = true;
+    	
+    	while(changed){
+    		ans.add(toList(nums));
+    		changed = nextPermutation(nums);
+
+    	}
+    	return ans;
+    	
+    }
+    
+    private List<Integer> toList(int[] nums){
+    	List<Integer> intList = new ArrayList<Integer>();
+    	for (int index = 0; index < nums.length; index++)
+    	{
+    	    intList.add(nums[index]);
+    	}
+    	return intList;
+    }
+    
+    private void swap(int[] nums,int i,int j){
+    	int temp = nums[i];
+    	nums[i] = nums[j];
+    	nums[j] = temp;
+    }
+    
+    private boolean nextPermutation(int[] nums){
+    	boolean changed = false;
+    	for(int i=nums.length-1;i>=1;i--){
+    		if(nums[i-1]<nums[i]){
+    			for(int j=nums.length-1;j>=1;j--){
+    				if(nums[i-1]<nums[j]){
+    					swap(nums,i-1,j);
+    					changed = true;
+    					Arrays.sort(nums,i,nums.length);
+    					break;
+    				}
+    			}
+    			break;
+    		}
+    	}
+
+    	return changed;
+    }
 }
+
+
+
