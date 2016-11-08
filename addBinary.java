37e289bd9ef07228b9327de679a38a74d8cda7b0
lynquantumman <lyn2511757@gmail.com>
2016-11-08 20:32:15 +0800

    better version

--- addBinary.java
+++ addBinary.java
@@ -1,8 +1,26 @@
 public class Solution {
     public String addBinary(String a, String b) {
-        int add1 = Integer.parseUnsignedInt(a,2);
-        int add2 = Integer.parseUnsignedInt(b,2);
-        int ans = add1+add2;
-        return Integer.toBinaryString(ans);
+    	int ans_length = Math.max(a.length(),b.length());
+    	int in = 0;
+    	String ans = "";
+    	int temp;
+        for(int i = 0;i<ans_length+1;i++){
+        	temp = in+get(a,i)+get(b,i);
+        	ans = Integer.toString(temp%2,10)+ans;
+        	in = temp/2;
+        }
+        if(ans.charAt(0)=='0'){
+        	ans = ans.substring(1);
+        }
+        return ans;
+    }
+    //reverse_ith is like 6543210
+    private int get(String input, int reverse_ith){
+    	if(input.length()>reverse_ith){
+    		return input.codePointAt(input.length()-1-reverse_ith)-'0';
+    	}
+    	else {
+    		return 0;
+    	}
     }
 }
