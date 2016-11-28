bb6a25d45a93aa7a078d294263808d091a740ca5
lynquantumman <lyn2511757@gmail.com>
2016-11-28 22:29:01 +0800

    no message

--- interleavingString.java
+++ interleavingString.java
@@ -1,27 +1,47 @@
+/**
+*_|_|_|
+*_|_|_|
+*use a structure like this to solve this question
+*let the row be the s2
+*let the collumn be s1
+*the way from the left up corner to the right down corner means s3
+*/
 public class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-    	if(s1.length()+s2.length()==s3.length())
-    		return recursiveJudge(s1,0,s2,0,s3,0);
-    	else return false;
-    }
-
-    private boolean recursiveJudge(String s1, int s1Position,
-    	String s2, int s2Position, String s3,int s3Position){
-
-    	if (s1.length()<=s1Position) {
-    		return s2.substring(s2Position).equals(s3.substring(s3Position));
+    	if(s1.length()+s2.length()!=s3.length()){
+    		return false;
     	}
-    	if (s2.length()<=s2Position) {
-    		return s1.substring(s1Position).equals(s3.substring(s3Position));
-    	}
+        boolean[][] dp = new boolean[s1.length()+1][s2.length()+1];
+        dp[0][0] = true;
+        // to simplify these code, we can decides the range of i,j 
+        // depending on if dp[i][j] is true
+        for (int i = 1;i<=s1.length();i++ ){
+        	if(s1.charAt(i-1)==s3.charAt(i-1)){
+        		dp[i][0] = true;
+        	}
+        	else break;
+        }
+        
+        for (int j = 1;j<=s2.length();j++ ){
+        	if(s2.charAt(j-1)==s3.charAt(j-1)){
+        		dp[0][j] = true;
+        	}
+        	else break;
+        }
+        
+        for (int i = 1;i<=s1.length();i++ ) {
+        	for (int j = 1;j<=s2.length();j++ ) {
+        		// s1的第i个字符
+        		if (s1.charAt(i-1)==s3.charAt(i+j-1)) {
+        			dp[i][j] = dp[i-1][j]||dp[i][j];
+        		}
+        		// s2的第j个字符
+        		if (s2.charAt(j-1)==s3.charAt(i+j-1)) {
+        			dp[i][j] = dp[i][j-1]||dp[i][j];
+        		}
+        	}
+        }
 
-    	if( s1.charAt(s1Position)==s3.charAt(s3Position)){
-        	if(recursiveJudge(s1,s1Position+1,s2,s2Position,s3,s3Position+1))
-        		return true;
-        }
-        if(s2.charAt(s2Position)==s3.charAt(s3Position)){
-        	return recursiveJudge(s1,s1Position,s2,s2Position+1,s3,s3Position+1);
-        }
-        return false;
+        return dp[s1.length()][s2.length()];
     }
 }
