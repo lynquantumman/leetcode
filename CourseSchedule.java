27d8a39475ef4cb92bb7f7a747ddd27d47aba2c3
lynquantumman <q2511757@126.com>
2017-04-23 22:30:23 +0800

    no message

--- CourseSchedule.java
+++ CourseSchedule.java
@@ -1,51 +1,72 @@
 public class Solution {
+    class preEle{
+        int start;
+        int end;
+        public preEle(int left,int right){
+            end = left;
+            start = right;
+        }
+    }
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         // delete lines are [-1,-1]
-    	TreeSet<Integer> startSet = new TreeSet<Integer>();
-        startPointSift(prerequisites, startSet);
-        int toRemoveStartPoint = 0;
-        while(!startSet.isEmpty()){
-        	while(!startSet.isEmpty()){
-        		
-        		toRemoveStartPoint = startSet.pollFirst();
-        		removeSpecificLine(prerequisites,toRemoveStartPoint);
-
-        	}
-        	startPointSift(prerequisites,startSet); 
+        HashSet<Integer> startSet = new HashSet<Integer>();
+        Comparator<preEle> cmp1 = new Comparator<preEle>(){
+            @Override
+            public int compare(preEle a, preEle b){
+                return a.start-b.start;
+            }
+        };
+        Comparator<preEle> cmp2  = new Comparator<preEle>(){
+            @Override
+            public int  compare(preEle a, preEle b){
+                return a.end-b.end;
+            }
+        };
+        cmp1 = cmp1.thenComparing(cmp2);
+        TreeSet<preEle> preConSet = new TreeSet<preEle>(cmp1);
+        for (int i = 0;i<prerequisites.length;i++) {
+            preConSet.add(new preEle(prerequisites[i][0],prerequisites[i][1]));
         }
-        return isEmpty(prerequisites);
+        
+        while(startPointSift(preConSet,startSet)){
+            
+            removeSpecificLine(preConSet,startSet);
+            
+        }
+        return preConSet.isEmpty();
     }
 
-    private void removeSpecificLine(int[][] prerequisites,int ele){
-    	for (int i =0;i<prerequisites.length ;i++ ) {
-    		if(prerequisites[i][1]==ele){
-    			prerequisites[i][0] = -1;
-    			
-    		}
-    			
-    	}
-
+    private void removeSpecificLine(TreeSet<preEle> preConSet,HashSet<Integer> startSet){
+        TreeSet<preEle> toRemove = new TreeSet<preEle>(preConSet);
+        for (preEle each:preConSet) {
+            if(startSet.contains(each.start) ){
+                toRemove.add(each);
+                
+            }
+            
+        }
+        preConSet.removeAll(toRemove);
+        startSet.clear();   
     }
 
-    private void startPointSift(int[][] prerequisites, TreeSet<Integer> startSet){
-    	
-    	TreeSet<Integer> endSet = new TreeSet<Integer>();
-    	for (int i = 0;i<prerequisites.length ;i++ ) {
-    		if (prerequisites[i][0]!=-1) {
-    			startSet.add(prerequisites[i][1]);
-    			endSet.add(prerequisites[i][0]);
-    		}
-    		
-    	}
-    	startSet.removeAll(endSet);
-    	
+    private Boolean startPointSift(TreeSet<preEle> preConSet, HashSet<Integer> startSet){
+        
+        
+        for (preEle each:preConSet) {
+            
+            startSet.add(each.start);
+                
+            
+        }
+
+        for (preEle each:preConSet) {
+            
+            startSet.remove(each.end);
+            
+        }
+        return !startSet.isEmpty();
+        
     }
 
-    private boolean isEmpty(int[][] prerequisites){
-    	for (int i = 0;i<prerequisites.length ;i++) {
-    		if(prerequisites[i][0]!=-1)
-    			return false;
-    	}
-    	return true;
-    }
+    
 }
